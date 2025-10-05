6be7b8ecd00177709d0555535b1c92137fc8c454
Erkan3034 <turguterkan55@gmail.com>
2025-10-06 01:15:01 +0300

    add HTML file and implement JavaScript functionality for greeting message

--- JS/01-hello-world.js
+++ JS/01-hello-world.js
@@ -7,4 +7,24 @@
 // alert(Merhaba Dunya)
 
 // console.log ile Merhaba Dunya:
-console.log(Merhaba Dunya)
+console.log("Merhaba Dunya")
+
+let title = document.getElementById("title");
+let time = 5;
+for (let i = 1; i <= time; i++) {
+    console.log(i + ". Merhaba Dunya");
+    function changeTitle() {
+        title.innerHTML = i + ". Merhaba Dunya";    
+        title.style.color = "blue";
+
+    }
+    setTimeout(changeTitle, i * 1000);
+    if (i == time) {
+        setTimeout(() => {
+            title.innerHTML = "Bitti";
+            title.style.color = "red";
+            alert("Sayim Bitti");
+        }, i * 1000);
+    }
+
+}
