69e1cf0aec13e5ee21d2855fc60e0e0343ab120c
Erkan3034 <turguterkan55@gmail.com>
2025-10-06 13:55:16 +0300

    refactor code structure by organizing functions and improving readability

---
+++ JS/02-degisken-tanimlama.js
@@ -0,0 +1,36 @@
+/* degisken tanimlari */
+
+//var ile degisken tanimlama
+var ad = "Erkan";
+var soyad = "Turgut";
+var yas = 20;
+var dogumTarihi = 1990;
+var meslek = "ogrenci";
+var sehir = "Istanbul";
+var ulke = "Turkiye";
+var email = "erkan@gmail.com";
+var telefon = "555-1234567";
+/*
+//let ile degisken tanimlama
+let ad = "Erkan";
+let soyad = "Turgut";
+let yas = 20;
+let dogumTarihi = 1990;
+let meslek = "ogrenci";
+let sehir = "Istanbul";
+let ulke = "Turkiye";
+let email = "erkan@gmail.com";
+let telefon = "555-1234567";
+
+//const ile degisken tanimlama
+const ad = "Erkan";
+const soyad = "Turgut";
+const yas = 20;
+const dogumTarihi = 1990;
+const meslek = "ogrenci";
+const sehir = "Istanbul";
+const ulke = "Turkiye";
+const email = "erkan@gmail.com";
+const telefon = "555-1234567";
+*/
+console.log(`****INFO*****\n ad: ${ad}\nsoyad: ${soyad}\nyas: ${yas}\ndogumTarihi: ${dogumTarihi}\nmeslek: ${meslek}\nsehir: ${sehir}\nulke: ${ulke}\nemail: ${email}\ntelefon: ${telefon}`);
