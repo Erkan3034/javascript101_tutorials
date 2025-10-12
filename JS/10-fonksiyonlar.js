c0642431444a3c035e0e568f9d6dbd8ea99cab9e
Erkan3034 <turguterkan55@gmail.com>
2025-10-12 23:54:08 +0300

    add functionality to hello function for displaying a message in the DOM with styled properties

--- JS/10-fonksiyonlar.js
+++ JS/10-fonksiyonlar.js
@@ -1,3 +1,16 @@
 function hello(){
     console.log("Hello World");
+    alert("Hello World");
+    let yazdir = document.getElementById("yazdir");
+    yazdir.style.color = "blue";
+    yazdir.style.fontSize = "16px";
+    yazdir.style.fontWeight = "normal";
+    yazdir.style.textAlign = "left";
+    yazdir.style.marginTop = "10px";
+    yazdir.style.marginBottom = "10px";
+    yazdir.style.marginLeft = "10px";
+    yazdir.innerHTML = "Merhaba Dünya";
 }
+
+
+
