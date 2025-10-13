18ee0d6d00a496e9e94cd0475438e882b3c2e8b0
Erkan3034 <turguterkan55@gmail.com>
2025-10-13 10:45:55 +0300

    refactor HTML and JavaScript for counter application to enhance styling and functionality

--- JS/exercises/01-fonk-dom-counter.js
+++ JS/exercises/01-fonk-dom-counter.js
@@ -1,3 +1,32 @@
 console.log("Counter uygulaması yüklendi.");
 
-let 
+const decrementButton = document.getElementById("decrement");
+const incrementButton = document.getElementById("increment");
+const valueDisplay = document.getElementById("value");
+const clearButton = document.getElementById("clear");  
+let value=0;
+
+const decrease = () => {
+     decrementButton.addEventListener("click", () => {
+        value--;
+        valueDisplay.textContent = value;
+        console.log("Decreased: ",value);
+    });
+};
+
+const increase = () => {
+    incrementButton.addEventListener("click", () => {
+        value++;
+        valueDisplay.textContent = value;
+        console.log("Increased: ",value);
+    });
+};
+
+clearButton.addEventListener("click", () => {
+    value = 0;
+    valueDisplay.textContent = value;
+    console.log("Cleared: ", value);
+});
+
+decrease();
+increase();
